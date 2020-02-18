d986a600f66fc0d9195723507349cab88da902df
unknown <ckdfo1995@gmail.com>
2020-02-18 17:35:59 +0900

    add logger, add Error Routing

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 var bodyParser = require('body-parser')
+const logger= require('morgan')
 
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
-
+app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-    console.log(req.headers);
     res.send('Hello World!'+req.query)
 })
 
@@ -21,5 +21,15 @@ app.post('/', (req, res) => {
     res.send(req.body)
 })
 
+app.use(function(req, res, next) {
+    res.status(404).send('Sorry cant find that!');
+});
+app.use((err, req, res, next)=>{
+    if (res.headersSent) {
+        return next(err);
+    }
+    res.status(500);
+    res.render('error', { error: err });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
