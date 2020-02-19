d57fcf3112e91d1d86c966208f0544ba114bdef5
unknown <ckdfo1995@gmail.com>
2020-02-19 15:23:50 +0900

    modify tdd, api design

--- index.js
+++ index.js
@@ -21,13 +21,97 @@ let users = [
         name: "Beta"
     }
 ]
-app.get('/users', (req, res) => {
-    res.json(users)
+
+app.get('/users', (req, res, next) => {
+    req.query.limit = req.query.limit || 10
+    const limitNum = parseInt(req.query.limit);
+    if(!limitNum) {
+        res.statusCode = 400
+        return res.send()
+    
+    }
+     
+    res.json(users.slice(0, limitNum))
 })
 
-app.post('/', (req, res) => {
-    res.send(req.body)
+app.get('/users/:id', (req, res, next)=>{
+    const id = parseInt(req.params.id)
+
+    if (!id) {
+        res.statusCode = 400
+        return res.send()
+    }
+    const filtered = users.filter((user)=>{
+        if (id === user.id) return true
+    })[0]
+
+    if (!filtered) {
+        return res.status(404).end()
+    }
+
+    res.json(filtered)
 })
+
+app.delete('/users/:id', (req, res, next)=>{
+    const id = parseInt(req.params.id)
+
+    if (!id) {
+        return res.status(400).end()
+    }
+
+    const deleted = users.filter(user => {
+        return id !== user.id
+    })
+
+    if (deleted.length === users.length) {
+        return res.status(404).end()
+    }
+
+    users = deleted
+    res.status(204).end()
+})
+
+app.post('/users', (req, res, next) => {
+    const name = req.body.name
+
+    if(!name) {
+        return res.status(400).end()
+    }
+
+    const overlap = users.filter(user => {
+        return user.name === name
+    })
+    if (overlap.length > 0) {
+        return res.status(409).end()
+    }
+
+    const tmpUser = {
+        id: Date.now(),
+        name: name
+    }
+
+    users.push(tmpUser)
+    res.status(201).json(tmpUser)
+})
+
+app.put('/users', (req, res, next) => {
+    const id = parseInt(req.body.id)
+    const name = req.body.name
+    if(!id || !name) {
+        return res.status(400).end()
+    }
+
+    const searched = users.filter(user => {
+        return user.id === id
+    })
+
+    if(searched.length == 0) {
+        return res.status(404).end()
+    }
+    res.status(200).json({id, name})
+})
+
+
 
 app.use(function(req, res, next) {
     res.status(404).send('Sorry cant find that!');
--- index.spec.js
+++ index.spec.js
@@ -3,18 +3,175 @@ const request = require('supertest')
 const app = require('./index')
 
 describe('GET /users', ()=>{
-    it('배열을 반환한다', (done)=>{
-        // assert.equal(1,1)
-        // (1).should.equal(2)
-        request(app)
-            .get('/users')
-            .end((err, res) =>{
-                if(err) throw err;
-                res.body.should.be.instanceof(Array).and.have.not.lengthOf(0)
-                res.body.forEach((user)=>{
-                    user.should.have.property('name')
+    describe('성공', ()=>{
+        it('배열을 반환한다', (done)=>{
+            // assert.equal(1,1)
+            // (1).should.equal(2)
+            request(app)
+                .get('/users')
+                .end((err, res) =>{
+                    if(err) throw err;
+                    res.body.should.be.instanceof(Array).and.have.not.lengthOf(0)
+                    res.body.forEach((user)=>{
+                        user.should.have.property('name')
+                    })
+                    done()
                 })
-                done()
-            })
+        })
+    
+        it('최대 limit 갯수만큼 응답한다. ', (done)=>{
+            request(app)
+                .get('/users?limit=2')
+                .end((err, res) =>{
+                    if(err) throw err;
+                    res.body.should.have.lengthOf(2)
+                    done()
+                })
+        })
     })
+    describe('실패', ()=>{
+        it('limit이 숫자가 아니면 400으로 응답한다.', (done)=>{
+            request(app)
+                .get('/users?limit=two')
+                .expect(400)
+                .end(done)
+        })
+    })
+    
+})
+
+describe('GET /users/:id', ()=>{
+    describe('성공', ()=>{
+        it('유저 객체를 반환한다.', done =>{
+            request(app)
+                .get('/users/1')
+                .end((err, res)=>{
+                    res.body.should.have.property('id', 1)
+                    done()
+                })
+        })
+    })
+    describe('실패', ()=>{
+        it('id가 숫자가 아니라면 400에러로 응답한다.', done=>{
+            request(app)
+                .get('/users/hello')
+                .expect(400)
+                .end(done)
+        })
+        it('id가 없을 경우에 404로 응답한다.', done=>{
+            request(app)
+                .get('/users/1000')
+                .expect(404)
+                .end(done)
+        })
+    })
+})
+
+describe('DELETE /users/:id', ()=>{
+    describe('성공', ()=>{
+        it('204로 응답한다.', done=>{
+            request(app)
+                .delete('/users/1')
+                .expect(204)
+                .end(done)
+        })
+    })
+    describe('실패', ()=>{
+        it('id가 숫자가 아니면 400에러로 응답한다.', done=>{
+            request(app)
+                .delete('/users/deleteHello')
+                .expect(400)
+                .end(done)
+        })
+        it('해당되는 아이디가 없을 경우, 404에러로 응답한다.', done=>{
+            request(app)
+                .delete('/users/1000')
+                .expect(404)
+                .end(done)
+        })
+    })
+    
+})
+
+describe('POST /users', () => {
+    describe('성공', () => {
+        it('201코드를 반환한다.', done => {
+            request(app)
+                .post('/users')
+                .send({name: 'Zeta'})
+                .expect(201)
+                .end(done)
+        })
+        it('생성된 유저 객체를 반환한다.', done => {
+            request(app)
+                .post('/users')
+                .send({name: 'Maga'})
+                .end((err, res) => {
+                    res.body.should.be.instanceof(Object)
+                    res.body.should.have.property("name", "Maga")
+                    done()
+                })
+        })
+    })
+
+    describe('실패', () => {
+        it('name 파라미터 없으면 400 응답한다.', done => {
+            request(app)
+                .post('/users')
+                .send({})
+                .expect(400)
+                .end(done)
+        })
+        it('name이 중복되면 409를 반환한다.', done => {
+            request(app)
+                .post('/users')
+                .send({name: 'Beta'})
+                .expect(409)
+                .end(done)
+        })
+
+    })
+    
+})
+
+
+describe('PUT /users', () => {
+    describe('성공', () => {
+        it('200코드를 반환한다.', done => {
+            request(app)
+                .put('/users')
+                .send({id: 2, name: 'Tera'})
+                .expect(200)
+                .end(done)
+        })
+        it('수정된 유저 객체를 반환한다.', done => {
+            request(app)
+                .put('/users')
+                .send({id: 2, name: 'Giga'})
+                .end((err, res) => {
+                    res.body.should.be.instanceOf(Object)
+                    res.body.should.have.properties('id', 'name')
+                    done()
+                })
+        })
+    })
+
+    describe('실패', () => {
+        it('해당되는 id가 없을 경우, 404에러 응답한다.', done => {
+            request(app)
+                .put('/users')
+                .send({id:4000, name:"test"})
+                .expect(404)
+                .end(done)
+        })
+        it('id 파라미터가 없을 경우, 400에러 응답한다.', done => {
+            request(app)
+                .put('/users')
+                .send({name: 'Test'})
+                .expect(400)
+                .end(done)
+        })
+
+    })
+    
 })
